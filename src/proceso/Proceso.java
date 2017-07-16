d3b99196ba4891996b45b221c465521f97f1c942
jaguilar992 <jaguilar992@gmail.com>
2017-07-16 17:00:35 -0600

    process

--- src/proceso/Proceso.java
+++ src/proceso/Proceso.java
@@ -7,6 +7,42 @@ public class Proceso {
 	private int nInstBloqueo; // Instrucción donde ocurre el bloqueo
 	private int tipoBloqueo;
 	
+	public static final int BLOQUEO_ES = 3;
+	public static final int BLOQUEO_DISK = 5;
+	
+	public static final int CICLOS_ES = 13;
+	public static final int CICLOS_DISK = 27;
 	// CAMPOS LOGICOS, ESTADO
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getPrioridad() {
+		return prioridad;
+	}
+	public void setPrioridad(int prioridad) {
+		this.prioridad = prioridad;
+	}
+	public int getnInst() {
+		return nInst;
+	}
+	public void setnInst(int nInst) {
+		this.nInst = nInst;
+	}
+	public int getnInstBloqueo() {
+		return nInstBloqueo;
+	}
+	public void setnInstBloqueo(int nInstBloqueo) {
+		this.nInstBloqueo = nInstBloqueo;
+	}
+	public int getTipoBloqueo() {
+		return tipoBloqueo;
+	}
+	public void setTipoBloqueo(int tipoBloqueo) {
+		this.tipoBloqueo = tipoBloqueo;
+	}
+	
 	
 }
